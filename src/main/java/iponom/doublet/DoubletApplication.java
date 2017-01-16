e382ee1bff60c35a9aa24988c3df0327ab35cce4
Ilya Ponomarev <Ilya.Ponomarev@centre-it.com>
2017-01-16 10:31:52 +0500

    ApplicationRunner has been separated from Application

--- src/main/java/iponom/doublet/DoubletApplication.java
+++ src/main/java/iponom/doublet/DoubletApplication.java
@@ -1,63 +1,19 @@
 package iponom.doublet;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.nio.file.Files;
-import java.nio.file.InvalidPathException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * @author Ilya Ponomarev.
  */
 @SpringBootApplication
 @Slf4j
-public class DoubletApplication implements ApplicationRunner {
-
-    @Value("${doublet.result.file.name}")
-    private String resultFileName;
-
-    @Autowired
-    private DuplicatesFinder duplicatesFinder;
+public class DoubletApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DoubletApplication.class, args);
     }
 
-    @Override
-    public void run(ApplicationArguments applicationArguments) throws Exception {
-        List<String> args = applicationArguments.getNonOptionArgs();
-        if (args.size() == 0) {
-            log.error("Set path to a directory as the first prameter");
-            return;
-        }
-        try {
-            Path path = Paths.get(args.get(0));
-            if (Files.isDirectory(path)) {
-                log.warn("Start searching");
-                Stream<List<String>> stream = duplicatesFinder.search(path);
-                List<String> result = stream
-                        .flatMap((list) -> Stream.concat(list.stream(), Stream.of("")))
-                        .collect(Collectors.toList());
-                Files.write(Paths.get(resultFileName), result);
-                log.warn("Searching has been finished. See " + resultFileName);
-            } else {
-                log.error(args.get(0) + " is not a directory");
-            }
-        } catch (InvalidPathException e) {
-            log.error(args.get(0) + " is an invalid path");
-        } catch (Exception e) {
-            log.error("Searching hasn't finished. ", e);
-        }
-    }
 
 }
